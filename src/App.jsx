235261bc97102365ea1de8d13c9a330fd4939040
Yatsenko Pavlo <pavelyasenko18@gmail.com>
2026-01-13 23:04:45 +0200

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,36 @@
+import { useState } from 'react';
+
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
+import { Header } from './HEADER/header';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+export const App = () => {
+  const [query, setQuery] = useState('');
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
-          </div>
-        </div>
+  let visibleMove = [...moviesFromServer];
+  const normalizeText = text => text.trim().toLowerCase();
+
+  if (normalizeText(query)) {
+    visibleMove = moviesFromServer.filter(
+      movie =>
+        normalizeText(movie.title).includes(normalizeText(query)) ||
+        normalizeText(movie.description).includes(normalizeText(query)),
+    );
+  }
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <Header
+          filterBy={newQuery => {
+            setQuery(newQuery);
+          }}
+        />
+        <MoviesList movies={visibleMove} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">Sidebar goes here</div>
     </div>
-
-    <div className="sidebar">Sidebar goes here</div>
-  </div>
-);
+  );
+};
